94a7c9d2e1c96fa105b1567656edc4bde994a65e
Marcus Galvin <marcusgalvin.student@careerdevs.com>
2019-08-02 15:58:14 -0400

    100%

--- caesar.c
+++ caesar.c
@@ -1,37 +1,61 @@
-#include <cs50.h>
 #include <stdio.h>
 #include <stdlib.h>
-//include these librarys for buttom loops
-#include <ctype.h>
 #include <string.h>
+#include <ctype.h>
+#include <cs50.h>
 
-int main(int argc, char **argv)
+int main(int argc, string argv[])
 {
+
+  //Make sure ther is only one command line argument
   if (argc != 2)
   {
-    printf("Usage: ./caesar k\n");
+    printf("Try Again\n");
     return 1;
   }
 
+  //Convert user input string to int
   int k = atoi(argv[1]);
+
+  //Make sure command line argument is non negative
   if (k < 0)
   {
-    printf("key must be pos\n");
+    printf("Try Again\n");
     return 1;
   }
-
-  string plaintext = get_string("plaintext: ");
-  //loop thru plain text, output encrypted text
-  for (int i = 0, len = strlen(plaintext); i < len; i++)
+  else
   {
-    if (islower(plaintext[i]))
-      printf("%c", (plaintext[i] - 'a' + k) % 26 + 'a');
-    else if (isupper(plaintext[i]))
-      printf("%c", (plaintext[i] - 'A' + k) % 26 + 'A');
 
-    else
-      printf("%c", plaintext[i]);
+    //Get user input string
+    string plainText = get_string("plaintext: ");
+
+    //Place to print cipher text
+    printf("ciphertext: ");
+
+    for (int i = 0, n = strlen(plainText); i < n; i++)
+    {
+
+      //Encrypt lower case
+      if
+        islower(plainText[i])
+        {
+          printf("%c", (((plainText[i] + k) - 97) % 26) + 97);
+        }
+      //encrypt upper case
+      else if
+        isupper(plainText[i])
+        {
+          printf("%c", (((plainText[i] + k) - 65) % 26) + 65);
+        }
+      //print anything else
+      else
+      {
+        printf("%c", plainText[i]);
+      }
+    }
+
+    printf("\n");
+
+    return 0;
   }
-  printf("\n");
 }
-//will fix
