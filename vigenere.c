b149a9633bb77a13f58e3aadd29f1b4ee6534fe7
Marcus Galvin <marcusgalvin.student@careerdevs.com>
2019-08-04 17:54:37 -0400

    vigenere started

--- vigenere.c
+++ vigenere.c
@@ -1,9 +1,25 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int main(void) {
+int main(void)
+{
 
+  // Check number of args
+  if (argc != 2)
+  {
+    printf("Wrong number of arguments. Please try again.\n");
 
+    return 1;
+  }
+  else
+  {
+    for (int i = 0, n = strlen(argv[1]); i < n; i++)
+    {
+      if (!isalpha(argv[1][i]))
+      {
+        printf("Must be alphabetic characters only. Please Try Again");
 
-  
-}
+        return 1;
+      }
+    }
+  }
